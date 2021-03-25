a8cb74210238fc20aa29cde7cd8e7d30657b0937
gisela <gi_selita2007@hotmail.com>
2021-03-24 23:05:36 -0300

    se agrega conexion firebase a environment.prod.ts

--- loginapp/src/environments/environment.prod.ts
+++ loginapp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC6W3UQP25B1R7Sx_Y-0SA9D4KmMISZtaQ",
+    authDomain: "login-6a495.firebaseapp.com",
+    projectId: "login-6a495",
+    storageBucket: "login-6a495.appspot.com",
+    messagingSenderId: "186767008806",
+    appId: "1:186767008806:web:0702f73f3835401484b2e7",
+    measurementId: "G-9ZFHSQZDFW"
+  }
 };
